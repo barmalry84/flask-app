bdd6a0ce6fbcb4c0330d61be5d2ce4573b4b8b25
Oleksandr Bukhanko <oleksandr.bukhanko@lotto24.de>
2024-11-16 10:33:49 +0000

    temp

--- iac/app-structure/providers.tf
+++ iac/app-structure/providers.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  backend "s3" {
-    bucket = "devops5-tf-state"
-    key    = "iac/app-structure.tfstate"
-    region = "eu-west-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "devops5-tf-state"
+#    key    = "iac/app-structure.tfstate"
+#    region = "eu-west-1"
+#  }
+#}
